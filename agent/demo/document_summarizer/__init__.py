5c25752c5ba0919cb1dbe44cc69dcb259d655da9
Curtis Faith <curtis.m.faith@gmail.com>
2017-09-24 20:49:42 -0400

    Cleanup spurious constants

--- agent/demo/document_summarizer/__init__.py
+++ agent/demo/document_summarizer/__init__.py
@@ -17,14 +17,6 @@ from sn_agent import ontology
 log = logging.getLogger(__name__)
 
 
-DOCUMENT_SUMMARIZER_ID          = 'deadbeef-aaaa-bbbb-cccc-000000000001'
-WORD_SENSE_DISAMBIGUATER_ID     = 'deadbeef-aaaa-bbbb-cccc-000000000002'
-FACE_RECOGNIZER_ID              = 'deadbeef-aaaa-bbbb-cccc-000000000003'
-TEXT_SUMMARIZER_ID              = 'deadbeef-aaaa-bbbb-cccc-000000000004'
-VIDEO_SUMMARIZER_ID             = 'deadbeef-aaaa-bbbb-cccc-000000000005'
-ENTITY_EXTRACTER_ID             = 'deadbeef-aaaa-bbbb-cccc-000000000006'
-
-
 class DocumentSummarizer(ModuleServiceAdapterABC):
     type_name = "DocumentSummarizer"
 
