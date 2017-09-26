23ebe28f2a828e71fec2a4ff9e1a8fc1aa733e31
Christian Jensen <christian@jensenbox.com>
2017-09-25 18:59:13 -0700

    more path

--- agent/sn_agent/ontology/settings.py
+++ agent/sn_agent/ontology/settings.py
@@ -1,8 +1,13 @@
+import os
+from pathlib import Path
+
 from sn_agent import SettingsBase
+
+THIS_DIR = Path(__file__).parent
 
 
 class OntologySettings(SettingsBase):
     def __init__(self, **custom_settings):
         self._ENV_PREFIX = 'SN_ONTOLOGY_'
-        self.CONFIG_FILE = 'sn_agent/ontology/ontology_mock.yml'
+        self.CONFIG_FILE = os.path.join(THIS_DIR, 'ontology_mock.yml')
         super().__init__(**custom_settings)
