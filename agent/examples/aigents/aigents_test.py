4cba18fcd030534da6a99a9ed02c828f53cff335
Anton Kolonin <akolonin@gmail.com>
2017-11-17 13:28:06 +0100

    Test edit

--- agent/examples/aigents/aigents_test.py
+++ agent/examples/aigents/aigents_test.py
@@ -2,7 +2,7 @@ import requests
 r = requests.get("https://aigents.com/al/?rss%20ai")
 print( r.status_code)
 print(r.headers)
-data1 = r.text
-print(data1)
+data = r.text
+print(data)
 print("Done")
 
