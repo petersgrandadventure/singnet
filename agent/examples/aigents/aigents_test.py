dd5652f334ea154310f40ff58f1b384dd4966eef
Anton Kolonin <akolonin@gmail.com>
2017-11-17 13:24:14 +0100

    Remove junk test

--- agent/examples/aigents/aigents_test.py
+++ agent/examples/aigents/aigents_test.py
@@ -1,10 +1,8 @@
-import http.client
-conn = http.client.HTTPConnection("aigents.com")
-conn.request("GET", "/al/?rss%20ai")
-r1 = conn.getresponse()
-print( r1.status, r1.reason)
-data1 = r1.read()
+import requests
+r = requests.get("https://aigents.com/al/?rss%20ai")
+print( r.status_code)
+print(r.headers)
+data1 = r.text
 print(data1)
-conn.close()
 print("Done")
 
